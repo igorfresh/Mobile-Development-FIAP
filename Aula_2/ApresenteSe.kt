7ee60aa51c9fe596b2435c558502173c67a231bf
Igor Silva <igormiguel37@gmail.com>
2024-02-22 20:25:36 -0300

    Declaração de variáveis em Kotlin

---
+++ Aula_2/ApresenteSe.kt
@@ -0,0 +1,14 @@
+//Declarando variáveis em Kotlin
+
+fun main() {
+
+    //Declaração das variáveis
+
+    var idade = 22
+    var altura = 1.71
+    var inicial = 'I'
+
+    //imprimir as informações do aluno interpolação de strings
+
+    println("Eu tenho ${idade} anos e ${altura} metros de altura e o meu nome começa com a inicial ${inicial}")
+}
